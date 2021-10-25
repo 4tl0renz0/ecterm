5b1e786589a01b8e1e4bd598ab23a39436b29464
Redpendrew <redpendrew@gmail.com>
2021-10-25 17:31:35 -0400

    src: added startup content to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     // major version number
     let vmajor = 0;
@@ -5,5 +7,9 @@ fn main() {
     let vminor = 1;
     // bugfix version number
     let vfix = 0;
-    println!("ECTerm v{0}.{1}.{2}", vmajor, vminor, vfix);
+    println!("ECTerm v{0}.{1}.{2}
+             Copyright (c) Redpendrew 2021
+             See Redpendrew @ \"https://github.com/Redpendrew\"
+             See the ECTerm repository @ \"https://github.com/Redpendrew/ECTerm\"
+             Input \"help\" for more commands.", vmajor, vminor, vfix);
 }
