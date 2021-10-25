1f614f52851d15b73edc75ca631cccda93400d49
Redpendrew <redpendrew@gmail.com>
2021-10-25 19:46:35 +0000

    Initialized with cargo

---
+++ ECTerm/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // major version number
+    let vmajor = 0;
+    // minor version number
+    let vminor = 1;
+    // bugfix version number
+    let vfix = 0;
+    println!("ECTerm v{0}.{1}.{2}", vmajor, vminor, vfix);
+}
