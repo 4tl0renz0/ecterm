5a5942971fc970450357c707623dbeea24f1ad7e
Redpendrew <redpendrew@gmail.com>
2021-10-26 16:43:25 +0000

    JuPhing this now so I can sync and fix later

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
-use std::io;
+use std::io::*;
+// use std::io::prelude::*;
+use std::process;
 
 fn main() {
+    println!("Initializing...");
+
     // major version number
-    let vmajor = 0;
+    let vmajor: u8 = 0;
     // minor version number
-    let vminor = 1;
+    let vminor: u8 = 1;
     // bugfix version number
-    let vfix = 0;
+    let vfix: u8 = 0;
+    // user input variable, "term"
+    let mut term = String::new();
     println!("ECTerm v{0}.{1}.{2}
-             Copyright (c) Redpendrew 2021
-             See Redpendrew @ \"https://github.com/Redpendrew\"
-             See the ECTerm repository @ \"https://github.com/Redpendrew/ECTerm\"
-             Input \"help\" for more commands.", vmajor, vminor, vfix);
+Copyright (c) Redpendrew 2021
+See Redpendrew @ \"https://github.com/Redpendrew\"
+See the ECTerm repository @ \"https://github.com/Redpendrew/ECTerm\"
+Input \"help\" for commands.", vmajor, vminor, vfix);
+    loop {
+        print!(">");
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut term).ok().expect("[ERROR: Failed to read line]");
+       /*
+        *if term == "exit" {
+        *    print!("Thank you for using ECTerm.");
+        *    process::exit(0x0100);
+        *};
+        */
+    }
 }
