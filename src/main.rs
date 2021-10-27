6795cdbc678b488c98423e18d42d9d617da604f2
Redpendrew <redpendrew@gmail.com>
2021-10-27 23:47:44 +0000

    Pushing now to sync later

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use std::io::*;
-use std::process::*;
+use std::process;
+
+/*
+   ____________________________
+  |____________________________|
+   |                          |
+   |   use std::fs;           |
+   |   use std::path::Path;   |
+   |__________________________|
+
+*/
 
 fn main() {
     println!("Initializing...");
-
     // major version number
     let vmajor: u8 = 0;
     // minor version number
@@ -12,18 +21,33 @@ fn main() {
     let vfix: u8 = 0;
     // user input variable, "term"
     let mut term = String::new();
-    println!("ECTerm v{0}.{1}.{2}
-Copyright (c) Redpendrew 2021
-See Redpendrew @ \"https://github.com/Redpendrew\"
-See the ECTerm repository @ \"https://github.com/Redpendrew/ECTerm\"
-Input \"help\" for commands.", vmajor, vminor, vfix);
+    // directory tracking variable
+    let mut dir = String::new();
+
+    println!("ECTerm v{0}.{1}.{2}\nCopyright (c) Redpendrew 2021\nSee Redpendrew @ \"https://github.com/Redpendrew\"\nSee the ECTerm repository @ \"https://github.com/Redpendrew/ECTerm\"\nInput \"help\" for commands.", vmajor, vminor, vfix);
     loop {
-        print!(">");
+        print!("{}>", dir);
         std::io::stdout().flush().unwrap();
         std::io::stdin().read_line(&mut term).ok().expect("[ERROR: Failed to read line]");
         term.remove(term.len() - 1);
+
+        if term.contains("cd") {
+            let term_parts: Vec<_> = term.split_whitespace().collect();
+            dir.push_str(term_parts[1]);
+        };
+
+        if term.contains("mkdir") {
+            let term_parts: Vec<_> = term.split_whitespace().collect();
+            
+        };
+
+        if term.contains("twm") {
+            let term_parts: Vec<_> = term.split_whitespace().collect();
+            println!("{0}", term_parts[1]);
+        };
+
         if term == "exit" {
-            print!("Thank you for using ECTerm.");
+            println!("Thank you for using ECTerm.");
             process::exit(0x0100);
         };
 
