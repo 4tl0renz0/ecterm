6fcaae50818bf2161c68356e2ee8a2f56f068268
Red <90254024+Redpendrew@users.noreply.github.com>
2021-11-04 12:26:14 -0400

    Create main.js

---
+++ docs/main.js
@@ -0,0 +1,30 @@
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function typeECTerm() {
+  await sleep(850);
+  progName.innerHTML = ">|      ";
+  await sleep(250);
+  progName.innerHTML = ">E|     ";
+  await sleep(200);
+  progName.innerHTML = ">EC|    ";
+  await sleep(300);
+  progName.innerHTML = ">ECT|   ";
+  await sleep(200);
+  progName.innerHTML = ">ECTe|  ";
+  await sleep(250);
+  progName.innerHTML = ">ECTer| ";
+  await sleep(300);
+  progName.innerHTML = ">ECTerm|";
+  await sleep(300);
+  for (let i = 0; i < 2; i++) {
+    progName.innerHTML = ">ECTerm ";
+    await sleep(900);
+    progName.innerHTML = ">ECTerm|";
+    await sleep(900);
+    i--
+  }
+}
+
+window.onload = typeECTerm();
