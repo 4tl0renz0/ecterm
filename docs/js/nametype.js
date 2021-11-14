8e09226ef96cc4552ab9a497726d27cf12c701e6
Redpendrew <redpendrew@gmail.com>
2021-11-13 20:15:40 -0500

    docs: removed em spaces

--- docs/js/nametype.js
+++ docs/js/nametype.js
@@ -3,23 +3,24 @@ function sleep(ms) {
 }
 
 async function typeECTerm() {
+  progName.innerHTML = ">_";
   await sleep(850);
-  progName.innerHTML = ">|      ";
+  progName.innerHTML = ">|";
   await sleep(250);
-  progName.innerHTML = ">E|     ";
+  progName.innerHTML = ">E|";
   await sleep(200);
-  progName.innerHTML = ">EC|    ";
+  progName.innerHTML = ">EC|";
   await sleep(300);
-  progName.innerHTML = ">ECT|   ";
+  progName.innerHTML = ">ECT|";
   await sleep(200);
-  progName.innerHTML = ">ECTe|  ";
+  progName.innerHTML = ">ECTe|";
   await sleep(250);
-  progName.innerHTML = ">ECTer| ";
+  progName.innerHTML = ">ECTer|";
   await sleep(300);
   progName.innerHTML = ">ECTerm|";
   await sleep(300);
   for (let i = 0; i < 2; i++) {
-    progName.innerHTML = ">ECTerm ";
+    progName.innerHTML = ">ECTerm_";
     await sleep(900);
     progName.innerHTML = ">ECTerm|";
     await sleep(900);
